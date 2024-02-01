0b6df2ef08000073095031be0010c741d99d6b33
varenya-gupta <153254554+varenya-gupta@users.noreply.github.com>
2024-02-01 08:45:01 +0530

    Update basic_openai_api_usage.py

--- basic_openai_api_usage.py
+++ basic_openai_api_usage.py
@@ -8,6 +8,9 @@ openai.api_key = ""
 
 #define list of messages
 messages = []
+#when using 'object_detection_on_images_with_YOLO&OPENAI.py' remove the comment tag from system message as it feeds the prompt to the ai.
+#system_msg = "I am a blind person requiring help in navigation through my daily life. For this i have created a object detection model that will give outputs like {'person': 5, 'car': 15} {'truck': 5, 'dog': 7} where the number beside each class is that class's amount and for inputs like this i want responses like there are 5 people and 15 cars in front, there are 5 trucks and 7 dogs in front of you. Only give the output and nothing else."
+#messages.append({"role": "system", "content": system_msg})
 
 #initialize and check if openai api key working
 print("Your new assistant is ready!")
